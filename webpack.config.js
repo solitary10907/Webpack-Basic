de43d16541091228741d5d62b89bb18f35b72b25
DESKTOP-29609U9\USER <solitary10907@gmail.com>
2022-04-11 23:43:30 +0800

    webpack-basic

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+//plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/template.html'), // template file
+      filename: 'index.html', // output file
+    }),
+    new CleanWebpackPlugin(),
+  ],
+}
